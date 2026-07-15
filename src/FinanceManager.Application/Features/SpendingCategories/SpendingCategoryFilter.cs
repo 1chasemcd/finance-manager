a73c3d92677e6ed31c5904bc6a9faabc88fc883a
Chase McDonald <1chasemcd@gmail.com>
2026-07-15 12:42:39 -0600

    remove unneeded properties from spending category filter

--- src/FinanceManager.Application/Features/SpendingCategories/SpendingCategoryFilter.cs
+++ src/FinanceManager.Application/Features/SpendingCategories/SpendingCategoryFilter.cs
@@ -5,10 +5,5 @@ namespace FinanceManager.Application.Features.SpendingCategories;
 
 public sealed record SpendingCategoryFilter
 {
-    [DefaultValue(0)]
-    public int Skip { get; init; }
-    [DefaultValue(50)]
-    [Range(1, 50)]
-    public int Take { get; init; } = 50;
     public string? NameContains { get; init; }
 }
