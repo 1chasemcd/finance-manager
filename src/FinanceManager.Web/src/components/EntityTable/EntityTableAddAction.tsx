2334af016856ea5693a9db92d79b04d7d489b980
Chase McDonald <1chasemcd@gmail.com>
2026-08-18 12:01:25 -0600

    Use some icons

--- src/FinanceManager.Web/src/components/EntityTable/EntityTableAddAction.tsx
+++ src/FinanceManager.Web/src/components/EntityTable/EntityTableAddAction.tsx
@@ -1,8 +1,9 @@
+import { Plus } from "lucide-react";
 import { LinkButton } from "../LinkButton";
 
 export default function EntityTableAddAction() {
   return (
-    <LinkButton to="./add" type="primary">
+    <LinkButton to="./add" type="primary" icon={<Plus size={16} />}>
       Add
     </LinkButton>
   );
--- src/FinanceManager.Web/src/components/EntityTable/EntityTableFilterAction.tsx
+++ src/FinanceManager.Web/src/components/EntityTable/EntityTableFilterAction.tsx
@@ -1,5 +1,6 @@
 import type { SearchEntityQuery } from "@/lib/types";
 import { Button, Drawer, Form, Space, type FormInstance } from "antd";
+import { Funnel } from "lucide-react";
 import { useState } from "react";
 
 type EntityTableFilterActionProps<TQuery extends SearchEntityQuery> = {
@@ -26,7 +27,9 @@ export default function EntityTableFilterAction<
 
   return (
     <>
-      <Button onClick={openFiltersForm}>Filter</Button>
+      <Button onClick={openFiltersForm} icon={<Funnel size={16} />}>
+        Filter
+      </Button>
       <Drawer
         title="Filter Results"
         onClose={() => setFiltersOpen(false)}
